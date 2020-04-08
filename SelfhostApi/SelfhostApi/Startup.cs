9a17120fa2f6168e3afa0628b1d839a2d97417d8
宋積杰 <zaq84723@gmail.com>
2020-04-08 20:28:22 +0800

    first self host web api

---
+++ SelfhostApi/SelfhostApi/Startup.cs
@@ -0,0 +1,40 @@
+﻿using NSwag.AspNet.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfhostApi
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            appBuilder.UseSwaggerUi3(typeof(Startup).Assembly, setting =>
+            {
+                setting.GeneratorSettings.DefaultUrlTemplate = "{controller}/{action}/{id?}";
+                setting.PostProcess = document =>
+                {
+                    document.Info.Title = "WebApi Sample";
+                };
+            });
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+        }
+    }
+}
